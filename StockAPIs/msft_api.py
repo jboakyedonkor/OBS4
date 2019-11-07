19305b46c0c86b331d63e2bd8639998916e65af0
Jesse Boakye-Donkor <jes142dd@ufl.edu>
2019-11-07 08:03:30 -0500

    created msft_tests.py

--- StockAPIs/msft_api.py
+++ StockAPIs/msft_api.py
@@ -1,23 +1,25 @@
 import requests, json
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 
-
+#api = Blueprint()
 app = Flask(__name__)
-app.config['DATA']
 
 @app.route('/msft/buy',methods=['POST'])
 def buy_share():
-    pass
+    return True
 
 @app.route('/msft/sell',methods=['POST'])
 def sell_share():
-    pass
+    return True
 
 @app.route('/msft/share_price',methods=['GET'])
 def get_price():
-    pass
+    return True
 
 @app.route('/msft/shares',methods=['GET'])
 def get_shares():
-    pass
+    return True
+
+if __name__ == "__main__":
+    app.run() 
---
+++ Tests/msft_tests.py
@@ -0,0 +1,5 @@
+from unittest import mock, TestCase, TestLoader, TestSuite 
+
+class MsftTestCase (TestCase):
+
+    pass
