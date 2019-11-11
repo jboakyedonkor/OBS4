4f30a6918d1405015821395f9191c2273596a0ea
Jesse Boakye-Donkor <jes142dd@ufl.edu>
2019-11-10 21:54:45 -0500

    added tests for get_quote and get_token

--- tests/msft_tests.py
+++ tests/msft_tests.py
@@ -10,27 +10,50 @@ import dotenv
 import jwt
 from flask import Flask
 sys.path.append(os.getcwd())
-from stockAPIs.msft_api import generate_token, verify_token
 
-class MsftTestCase (unittest.TestCase):
+from stockAPIs.msft_api import generate_token, verify_token, get_quote, get_token
+
+class MsftHelperTestCase (unittest.TestCase):
     def setUp(self):
         dotenv.load_dotenv(
             dotenv_path=".{}config{}.env".format(
                 os.sep, os.sep))
         self.secret_key = os.getenv('SECRET_KEY')
         self.api_key = os.getenv('API_KEY')
+        
+    def test_get_quote(self):
+        api_url ="https://sandbox.tradier.com/v1/markets/quotes"
 
-    def test_get_shares(self):
-        pass
+        params = {'symbols': 'MSFT'}
 
-    def test_get_price(self):
-        pass
+        headers = {'Accept': 'application/json',
+               'Authorization': 'Bearer ' +self.api_key
+               }
 
-    def test_post_buy(self):
-        pass
+        api_response =get_quote()
 
-    def test_post_sell(self):
-        pass
+        test_response = requests.get(api_url,params=params, headers=headers)
+        test_json = test_response.json()
+
+        self.assertEqual(api_response,test_json['quotes']['quote'],"the Tradier API do not get the same response")
+
+
+    def test_get_token(self):
+        test_header = {'Authorization': 'check'}
+        test_header2 = {'Authorizationd': 'check'}
+
+        token = get_token(test_header)
+        token2 = get_token(test_header2)
+
+        self.assertEqual(
+            token,
+            'check',
+            'the authorization shoud be \'check\'')
+
+        self.assertEqual(
+            token2,
+            None,
+            'The was not authorization header or an empty header')
 
     def test_generate_token(self):
 
@@ -126,11 +149,30 @@ class MsftTestCase (unittest.TestCase):
                          "should have been rejected as an invalid signature")
 
 
+class MsftRoutesTestCase(unittest.TestCase):
+
+    def test_get_shares(self):
+        pass
+
+    def test_get_price(self):
+        pass
+
+    def test_post_buy(self):
+        pass
+
+    def test_post_sell(self):
+        pass
+
+
 if __name__ == "__main__":
 
-    cmd = ['python3', '..' + os.sep + 'StockAPIs' + os.sep + 'msft_api.py']
+    cmd = ['python3', '.' + os.sep + 'StockAPIs' + os.sep + 'msft_api.py']
+
+    if os.name == 'nt':
+        cmd[0] = 'python'
+
     api_proc = Popen(cmd)
-    time.sleep(0.2)
-    unittest.main(exit=False)
+    time.sleep(0.7)
+    unittest.main(exit=False, verbosity=3)
     api_proc.kill()
     time.sleep(0.2)
