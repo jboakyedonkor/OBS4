c2a134f69e1baae5bc6715e89a8dcc0b15a1bee3
Giovanni Gaspard <giovannigaspard@Giovannis-MacBook-Pro.local>
2019-11-16 13:08:08 -0500

    Added to yaml

--- apis/aapl_helper.py
+++ apis/aapl_helper.py
@@ -8,13 +8,13 @@ dotenv.load_dotenv(dotenv_path=".{}config{}.env".format(os.sep, os.sep))
 
 def intialize_firebase():
     config = {
-        "apiKey": os.getenv("FIRE_API_KEY"),
-        "authDomain": os.getenv("AUTH_DOMAIN"),
-        "databaseURL": os.getenv("DATABASE_URL"),
-        "projectId": os.getenv("PROJECT_ID"),
-        "storageBucket": os.getenv("STORAGE_BUCKET"),
-        "messagingSenderId": os.getenv("MESS_SENDER_ID"),
-        "appId": os.getenv("APP_ID")
+        "apiKey": os.getenv("AAPL_FIRE_API_KEY"),
+        "authDomain": os.getenv("AAPL_AUTH_DOMAIN"),
+        "databaseURL": os.getenv("AAPL_DATABASE_URL"),
+        "projectId": os.getenv("AAPL_PROJECT_ID"),
+        "storageBucket": os.getenv("AAPL_STORAGE_BUCKET"),
+        "messagingSenderId": os.getenv("AAPL_MESS_SENDER_ID"),
+        "appId": os.getenv("AAPL_APP_ID")
     }
 
     firebase = pyrebase.initialize_app(config)
