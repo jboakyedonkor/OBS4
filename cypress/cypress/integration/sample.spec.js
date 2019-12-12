17aff291b9805448c3b2dbb8c280cfa298b2e0f7
Giovanni Gaspard <giovannigaspard@Giovannis-MBP.lan>
2019-12-12 01:24:38 -0500

    Cypress Test

--- cypress/cypress/integration/sample.spec.js
+++ cypress/cypress/integration/sample.spec.js
@@ -1,27 +1,81 @@
 /// <reference types="Cypress" />
 
 context('OBS',() =>{
-    const email = "test1@gmail.com"
-    const password = "welp"
-
-    const badEmail = "test2@gmail.com"
+    const username = "assign2"
+    const email = "assign2@gmail.com"
+    const password = "ggggggggg"
    
-    beforeEach(() => {
-        cy.visit('http://localhost:5000/')
-      })
+    const badEmail = "test1@gmail.com"
+    const badPassword = "www"
+    const fundAmount = 10000
+    const aaplShareAmount = 20
+   
+   
+    
     it('It has a title', () => {
+        cy.visit('http://localhost:5000/')
         cy.contains("OBS")
     })
-    it('Happy Path Success',() =>{
+    it('Register for an account',() => {
+
+        cy.get('#register').click() 
+        cy.get('#username').type(username) 
+        cy.get('#email').type(email)
+        cy.get('#password').type(password)
+        cy.get('#confirm_password').type(password)
+        cy.get('#submit').click()
+        cy.contains("Your account has been created! You are now able to log in")
+    })
+    // it('Failure Case to login/ Bad Password',() =>{
+    //     cy.get('#email').type(badEmail)
+    //     cy.get('#password').type(badPassword)
+    //     cy.get('#submit').click()
+    //     cy.contains("Login Unsuccessful. Please check email and password")
+    // })
+    it('Login with just registered account Success',() =>{
+        cy.get('#login').click() 
         cy.get('#email').type(email)
         cy.get('#password').type(password)
         cy.get('#submit').click()
-        cy.contains("Logout")
+        cy.get('#dashboard').click() 
+        
+        // cy.contains("Logout")
     })
-    it('Failure Case',() =>{
-        cy.get('#email').type(badEmail)
-        cy.get('#password').type(password)
-        cy.get('#submit').click()
-        cy.contains("Logout")
-    })
-})
+    // it('Add funds',() =>{
+    //     cy.get('#login').click() 
+    //     cy.get('#email').type(email)
+    //     cy.get('#password').type(password)
+    //     cy.get('#submit').click()
+    //     cy.get('#dashboard').click() 
+        
+    //     cy.get('#addFundsButton').click() 
+    //     cy.get('#fundAmount').type(fundAmount)
+    //     cy.contains('Confirm').click()
+    //     cy.contains('Cash:'+fundAmount)
+    // })
+    // it('Make a purchase',() =>{
+    //     cy.get('#aaplShareAmount').type(aaplShareAmount)
+    //     cy.get('#aaplBuyButton').click() 
+    //     cy.wait(2000)
+    //     cy.get('totAAPLShares').contains(aaplShareAmount)
+        
+        
+    // })
+//     it('Sell Shares',() =>{
+//         cy.get('#aaplShareAmount').type(aaplShareAmount-10)
+//         cy.get('#aaplSellButton').click() 
+//         cy.wait(6000)
+//         cy.get('totAAPLShares').contains(aaplShareAmount-10)
+        
+        
+
+      it('Transaction Page',() =>{
+        cy.contains('Transactions').click() 
+        
+        // cy.get('#login').click() 
+        // cy.get('#email').type(email)
+        // cy.get('#password').type(password)
+        // cy.get('#submit').click()
+        
+ })
+     })
