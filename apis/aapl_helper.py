934fbc427b9b50f303a458c69471de20f3e135bb
Giovanni Gaspard <giovannigaspard@Giovannis-MacBook-Pro.local>
2019-12-12 19:27:19 -0500

    Added dash stuff

--- apis/aapl_helper.py
+++ apis/aapl_helper.py
@@ -9,13 +9,13 @@ dotenv.load_dotenv(dotenv_path=".{}config{}.env".format(os.sep, os.sep))
 
 def intialize_firebase():
     config = {
-        "apiKey": os.getenv("FIREBASE_API_KEY"),
-        "authDomain": os.getenv("FIREBASE_AUTH_DOMAIN"),
-        "databaseURL": os.getenv("FIREBASE_DB_URL"),
-        "projectId": os.getenv("FIREBASE_PROJECT_ID"),
-        "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET"),
-        "messagingSenderId": os.getenv("FIREBASE_MSG_SENDER_ID"),
-        "appId": os.getenv("FIREBASE_APP_ID")
+        "apiKey": os.getenv("MSFT_FB_API_KEY"),
+        "authDomain": os.getenv("MSFT_FB_AUTH_DOMAIN"),
+        "databaseURL": os.getenv("MSFT_FB_DB_URL"),
+        "projectId": os.getenv("MSFT_FB_PROJECT_ID"),
+        "storageBucket": os.getenv("MSFT_FB_STORAGE_BUCKET"),
+        "messagingSenderId": os.getenv("MSFT_FB_SENDER_ID"),
+        "appId": os.getenv("MSFT_FB_APP_ID")
     }
 
     firebase = pyrebase.initialize_app(config)
