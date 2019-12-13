0073d3788a3957733b2e6aa4ff9e01bbedd78762
Steven Medina <stevenmedina@ufl.edu>
2019-12-12 21:49:41 -0500

    lol

--- apis/fb_api.py
+++ apis/fb_api.py
@@ -15,13 +15,13 @@ dotenv.load_dotenv(dotenv_path='.\\config\\.env')
 app = Flask(__name__)
 
 config = {
-    "apiKey": os.getenv('FIREBASE_API_KEY'),
-    "authDomain": os.getenv('FIREBASE_AUTH_DOMAIN'),
-    "databaseURL": os.getenv('FIREBASE_DB_URL'),
-    "projectId": os.getenv('FIREBASE_PROJECT_ID'),
-    "storageBucket": os.getenv('FIREBASE_STORAGE_BUCKET'),
-    "messagingSenderId": os.getenv('FIREBASE_MSG_SENDER_ID'),
-    "appId": os.getenv('FIREBASE_APP_ID'),
+    "apiKey": os.getenv('MSFT_FB_API_KEY'),
+    "authDomain": os.getenv('MSFT_FB_AUTH_DOMAIN'),
+    "databaseURL": os.getenv('MSFT_FB_DB_URL'),
+    "projectId": os.getenv('MSFT_FB_PROJECT_ID'),
+    "storageBucket": os.getenv('MSFT_FB_STORAGE_BUCKET'),
+    "messagingSenderId": os.getenv('MSFT_FB_SENDER_ID'),
+    "appId": os.getenv('MSFT_FB_APP_ID'),
 }
 
 fire_db = pyrebase.initialize_app(config).database()
@@ -160,7 +160,7 @@ def sell_share():
             'payment': num_owned * price,
             'trans_type': 'SELL',
             'amount': num_owned,
-            'user': username,
+            'username': username,
             'price': price})
 
     return jsonify(
